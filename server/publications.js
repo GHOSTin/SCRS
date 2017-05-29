39c1b849066313fe4026402fdf7292f52d3d03d6
GHOSTin <lwhiteghostl@gmail.com>
2017-05-29 18:13:18 +0600

    Раздел студентов

--- server/publications.js
+++ server/publications.js
@@ -1,9 +1,15 @@
+import {Students} from '../lib/collections/students.js'
+
 Meteor.publish('posts', function() {
   return Posts.find();
 });
 
 Meteor.publish('userList', function () {
   return Meteor.users.find({});
+});
+
+Meteor.publish('studentsList', function () {
+    return Students.find();
 });
 
 Meteor.publish(null, function (){
