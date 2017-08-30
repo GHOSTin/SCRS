cb4280f0ef006fda31c63e917ee7644e57c3c735
GHOST home <lwhiteghostl@gmail.com>
2017-08-30 19:03:53 +0500

    Удаление пользователей

--- client/views/users/userItem.js
+++ client/views/users/userItem.js
@@ -4,3 +4,19 @@ Template.userItem.helpers({
     return Roles.getRolesForUser(this._id).join(', ')
   }
 });
+
+Template.userItem.events({
+    'click .delete': (event)=>{
+        event.preventDefault();
+        let userId = $(event.currentTarget).attr('data-user-id');
+        if(confirm('Вы действительно хотите удалить пользователя?')){
+            Meteor.call('deleteUser', userId, (error, result)=>{
+                if (error) {
+                    console.log(error);
+                    return false;
+                }
+                Bert.alert( 'Удалено!', 'success', 'growl-top-right' );
+            })
+        }
+    }
+});
